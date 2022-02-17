519d082ab6fd8f86ab21580521fc34cc41dc58f5
Kamar <kmprojects1998@gmail.com>
2022-02-17 14:27:23 +0300

    Change decimal to double

---
+++ API/Migrations/20220217105310_database9.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class database9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "MonthlyIncome",
+                table: "Clients",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MonthlyIncome",
+                table: "Clients",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
